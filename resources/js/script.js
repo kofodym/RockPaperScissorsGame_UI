42095ae1323672b266e7200f830241089ffc11a7
KofoCole <kofodym07@yahoo.com>
2023-10-03 19:02:52 +0100

    create variables that select ID and Classes from your HTML file

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,2 +1,10 @@
 // Make a choice by selecting from a node list of buttons (choices)
 const buttons = document.querySelectorAll("button");
+
+// Connect and display results in HTML by selecting the corresponding elements
+const choices = document.querySelector(".choices");
+const roundNo = document.querySelector(".roundNo");
+const playerScore = document.querySelector(".playerScore");
+const compScore = document.querySelector(".compScore");
+const wonOrLose = document.querySelector("#wonOrLose");
+const overalWinner = document.querySelector("#overalWinner");
